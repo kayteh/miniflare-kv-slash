6ad725c6684db8af589d34a7d5e23bb98f2accac
Katalina Okano <git@kat.cafe>
2022-01-30 14:33:29 -0500

    minimal slash-in-kv-key repro

---
+++ slash-in-kv-key.mjs
@@ -0,0 +1,11 @@
+import { Miniflare } from "miniflare";
+
+const mf = new Miniflare({
+    kvNamespaces: ["TEST_NAMESPACE"],
+    kvPersist: true,
+    script: `addEventListener("fetch", () => new Response(null));`,
+});
+
+const ns = await mf.getKVNamespace("TEST_NAMESPACE");
+await ns.put("/foo/bar", "baz");
+await ns.put("/foo", "bar");
